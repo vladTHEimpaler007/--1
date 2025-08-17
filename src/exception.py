37125102c0419cb18f869fbf0ea1f75eeaa404c0
sayantan <gsayantan2408@gmail.com>
2025-08-17 13:16:40 +0530

    EDA, Feature Engg. and Model Comparisons

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,13 @@
 import sys  
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_Detail:sys):
      _,_,exc_tb = error_Detail.exc_info()
      file_name=exc_tb.tb_frame.f_code.co_filename
-     error_meesage = "Error Occured in python script name[{0}] line number [{1}] error message{{2}}".format(
+     error_message = "Error Occured in python script name[{0}] line number [{1}] error message{{2}}".format(
           file_name,exc_tb.tb_lineno,str(error)
           )
-     return error_meesage
+     return error_message
      
 
 class CustomException(Exception):
